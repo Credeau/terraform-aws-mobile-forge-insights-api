05f08b12f6bb259a2ed077119ab17077296d5c40
Harshit Sharma <brown.hash@outlook.com>
2025-07-17 16:29:30 +0530

    fix acl arn references

--- waf.tf
+++ waf.tf
@@ -54,7 +54,7 @@ resource "aws_wafv2_web_acl" "main" {
 
     statement {
       ip_set_reference_statement {
-        arn = aws_wafv2_ip_set.main.arn
+        arn = aws_wafv2_ip_set.main[0].arn
       }
     }
 
@@ -77,12 +77,12 @@ resource "aws_wafv2_web_acl_association" "main" {
   count = var.use_public_endpoint ? 1 : 0
 
   resource_arn = aws_lb.main.arn
-  web_acl_arn  = aws_wafv2_web_acl.main.arn
+  web_acl_arn  = aws_wafv2_web_acl.main[0].arn
 }
 
 resource "aws_wafv2_web_acl_logging_configuration" "main" {
   count = var.use_public_endpoint ? 1 : 0
 
   log_destination_configs = [aws_cloudwatch_log_group.waf.arn]
-  resource_arn            = aws_wafv2_web_acl.main.arn
+  resource_arn            = aws_wafv2_web_acl.main[0].arn
 }
