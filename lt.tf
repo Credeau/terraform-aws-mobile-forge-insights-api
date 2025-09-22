7fdb6e669e51f19898a67624a1b89e1a695c0f1c
Harshit Sharma <brown.hash@outlook.com>
2025-09-22 16:59:07 +0530

    add cache expiry period and webhook url for fast features

--- lt.tf
+++ lt.tf
@@ -103,6 +103,8 @@ resource "aws_launch_template" "main" {
       predictors_payday_score_lgb_model_v3_path = var.predictors_payday_score_lgb_model_v3_path
       emi_score_lgb_model_v2_path               = var.emi_score_lgb_model_v2_path
       predictors_emi_score_lgb_model_v2_path    = var.predictors_emi_score_lgb_model_v2_path
+      fast_featurization_webhook                = var.fast_featurization_webhook
+      cache_expiration_minutes                  = var.cache_expiration_minutes
     }
   ))
 
